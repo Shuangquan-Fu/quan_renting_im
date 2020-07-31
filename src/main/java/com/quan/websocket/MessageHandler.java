53e85da620c580c853e501c7c30227c59a8f1a1f
shufu <fsq19981210@gmail.com>
2020-07-31 00:54:59 -0400

    setting websocket service

--- src/main/java/com/quan/websocket/MessageHandler.java
+++ src/main/java/com/quan/websocket/MessageHandler.java
@@ -1,9 +1,13 @@
 package com.quan.websocket;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.quan.dao.MessageDao;
+import com.quan.pojo.Message;
+import com.quan.pojo.UserData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
@@ -24,5 +28,27 @@ public class MessageHandler extends TextWebSocketHandler {
         // 将当前用户的session放置到map中，后面会使用相应的session通信
         SESSIONS.put(uid, session);
     }
-    
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage Tmessage) throws Exception {
+        Long uid  = (Long) session.getAttributes().get("uid");
+        JsonNode jsonNode = MAPPER.readTree(Tmessage.getPayload());
+        Long toId = jsonNode.get("toId").asLong();
+        String msg = jsonNode.get("msg").asText();
+        Message message = Message.builder()
+                .from(UserData.USER_MAP.get(uid))
+                .to(UserData.USER_MAP.get(toId))
+                .msg(msg)
+                .build();
+        message = this.messageDao.saveMessage(message);
+// 判断to用户是否在线
+        WebSocketSession toSession = SESSIONS.get(toId);
+        if(toSession != null && toSession.isOpen()){
+//TODO 具体格式需要和前端对接
+            toSession.sendMessage(new
+                    TextMessage(MAPPER.writeValueAsString(message)));
+// 更新消息状态为已读
+            this.messageDao.updateMessageState(message.getId(), 2);
+        }
+    }
 }
