7bcb2d030acc30732d1c4c2ac5dd3c1bcc822ac4
shufu <fsq19981210@gmail.com>
2020-08-01 18:34:10 -0400

    setting RocketMQ

--- src/main/java/com/quan/websocket/MessageHandler.java
+++ src/main/java/com/quan/websocket/MessageHandler.java
@@ -5,7 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.quan.dao.MessageDao;
 import com.quan.pojo.Message;
 import com.quan.pojo.UserData;
+import org.apache.rocketmq.spring.annotation.MessageModel;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -15,9 +21,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class MessageHandler extends TextWebSocketHandler {
+@RocketMQMessageListener(topic = "quan-im-send-message-topic",
+        consumerGroup = "quan-im-consumer",
+        messageModel = MessageModel.BROADCASTING,
+        selectorExpression = "SEND_MSG")
+public class MessageHandler extends TextWebSocketHandler implements RocketMQListener<String> {
     @Autowired
     private MessageDao messageDao;
+
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
+
     private static final ObjectMapper MAPPER = new ObjectMapper();
     private static final Map<Long, WebSocketSession> SESSIONS = new HashMap<>();
 
@@ -41,6 +55,7 @@ public class MessageHandler extends TextWebSocketHandler {
                 .msg(msg)
                 .build();
         message = this.messageDao.saveMessage(message);
+        String msgStr = MAPPER.writeValueAsString(message);
         // 判断to用户是否在线
         WebSocketSession toSession = SESSIONS.get(toId);
         if(toSession != null && toSession.isOpen()){
@@ -48,6 +63,30 @@ public class MessageHandler extends TextWebSocketHandler {
             toSession.sendMessage(new TextMessage(MAPPER.writeValueAsString(message)));
         // 更新消息状态为已读
             this.messageDao.updateMessageState(message.getId(), 2);
+        } else {
+            org.springframework.messaging.Message mqMessage = MessageBuilder
+                    .withPayload(msgStr)
+                    .build();
+// topic:tags 设置主题和标签
+            this.rocketMQTemplate.send("haoke-im-send-message-topic:SEND_MSG",
+                    mqMessage);
+        }
+    }
+
+    @Override
+    public void onMessage(String msg) {
+        try {
+            JsonNode jsonNode = MAPPER.readTree(msg);
+            Long toId = jsonNode.get("to").get("id").longValue();
+// 判断to用户是否在线
+            WebSocketSession toSession = SESSIONS.get(toId);
+            if (toSession != null && toSession.isOpen()) {
+                toSession.sendMessage(new TextMessage(msg));
+// 更新消息状态为已读
+                this.messageDao.updateMessageState(new ObjectId(jsonNode.get("id").asText()), 2);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
